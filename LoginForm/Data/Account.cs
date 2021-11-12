8afdcafc28be55d5e4d4c318a59639c6aff66ebf
predrag.nikolikj <pedza.nikolic@outlook.com>
2021-11-12 09:53:13 +0100

    Add project files.

---
+++ LoginForm/Data/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginForm.Data
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ LoginForm/Data/IRepository/IAccountData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginForm.Data.IRepository
+{
+    public interface IAccountData
+    {
+        public Account GetById(int id);
+        public IEnumerable<Account> GetAccounts();
+        public Account GetUserByName(string username);
+    }
+
+    public class InMemoryData : IAccountData
+    {
+        readonly List<Account> accounts;
+
+        public InMemoryData()
+        {
+            accounts = new List<Account>
+            {
+                new Account { Id = 1, Username = "User1", Password = "Pass1"},
+                new Account { Id = 2, Username = "User2", Password = "Pass2"},
+                new Account { Id = 3, Username = "User3", Password = "Pass3"},
+            };
+        }
+        public Account GetById(int id)
+        {
+            return accounts.SingleOrDefault(r => r.Id == id);
+        }
+        
+        public IEnumerable<Account> GetAccounts()
+        {
+            return accounts;
+        }
+        
+        public Account GetUserByName(string username)
+        {
+            var query = from r in accounts
+                   where r.Username.StartsWith(username)
+                   orderby r.Username
+                   select r;
+            
+            return query.First();
+        }
+    }
+}
