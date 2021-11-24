d64c30a8b6334134327374f066f89753a7a96eb8
dzape <pedza.nikolic@outlook.com>
2021-11-24 15:54:52 +0100

    AccountTable

---
+++ src/WeatherApp/Data/AccountTable.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Accounts] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [Username]     NVARCHAR (MAX) NULL,
+    [Password]     NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
