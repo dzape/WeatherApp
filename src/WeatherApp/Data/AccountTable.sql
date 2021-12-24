f44781f3240bbc860b406f75d0aacf6f7b9a1150
Pedza <47067384+dzape@users.noreply.github.com>
2021-12-24 10:17:44 +0100

    Delete src directory

--- src/WeatherApp/Data/AccountTable.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Accounts] (
-    [Id]           INT            IDENTITY (1, 1) NOT NULL,
-    [Username]     NVARCHAR (MAX) NULL,
-    [Password]     NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
